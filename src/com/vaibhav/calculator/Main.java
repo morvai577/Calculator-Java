f5a631dbf897f7d704782a03f375e76a52d1c239
Vaibhav Mor <mor.vaibhav577@gmail.com>
2017-03-08 21:34:37 +1300

    Learned about basic operands, operators and operator precedence.

--- src/com/vaibhav/calculator/Main.java
+++ src/com/vaibhav/calculator/Main.java
@@ -3,6 +3,29 @@ package com.vaibhav.calculator;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Operands
+        int valA = 21;
+        int valB = 6;
+        int valC = 3;
+        int valD = 1;
+
+        //Operations
+        int result1 = valA - valB / valC;
+
+        int result2 = (valA - valB) / valC; // Operator Precedence
+
+        System.out.println(result1);
+        System.out.println(result2);
+
+        int result3 = valA / valC * valD + valB;
+
+        int result4 = valA / (valC * (valD + valB));
+
+        System.out.println(result3);
+        System.out.println(result4);
+
+
+
     }
 }
