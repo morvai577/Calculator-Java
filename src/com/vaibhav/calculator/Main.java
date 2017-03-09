61786db1a985feec329eba0407518501679b3aaf
Vaibhav Mor <mor.vaibhav577@gmail.com>
2017-03-09 17:36:52 +1300

    Implemented arrays, so now calculator can carry out multiple operations at once and also implemented switch logic instead of if/else logic to clean up code.

--- src/com/vaibhav/calculator/Main.java
+++ src/com/vaibhav/calculator/Main.java
@@ -5,35 +5,43 @@ public class Main {
     public static void main(String[] args) {
 
         // Operands
-        double val1 = 100.0d;
-        double val2 = 50.0d;
-        double result;
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
 
 
-        // Operators
-        char opCode = 'a';
+        for(int i = 0; i < opCodes.length; i++) {
 
-        // Program Logic
-        if(opCode == 'a')
-            result = val1 + val2;
-        else if(opCode == 's')
-            result = val1 - val2;
-        else if(opCode == 'd')
-            if (val2 != 0.0d) // Check for zero division
-                result = val1 / val2;
-            else {
-                System.out.println("Error - zero division error");
-                result = 0.0d;
+            // Program Logic
+            switch (opCodes[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'd':
+                    if (rightVals[i] != 0.0d) // Check for zero division
+                        results[i] = leftVals[i] / rightVals[i];
+                    else {
+                        System.out.println("Error - zero division error");
+                        results[i] = 0.0d;
+                    }
+                    break;
+                case'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                default:
+                    System.out.println("Error - invalid opCode"); // Error Handling
+                    results[i] = 0.0d;
             }
-        else if(opCode == 'm')
-            result = val1 * val2;
-        else {
-            System.out.println("Error - invalid opCode"); // Error Handling
-            result = 0.0d;
+
         }
 
-        System.out.println(result);
-
+        for (double theResult: results) {
+            System.out.println("result = " + theResult);
+        }
 
     }
 }
