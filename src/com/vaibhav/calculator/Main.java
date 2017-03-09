198d39c9575babe0576d13ed9559f7b477a2be18
Vaibhav Mor <mor.vaibhav577@gmail.com>
2017-03-09 17:16:58 +1300

    Added program logic, now if you want to add, subtract, divide or multiply, then update the opCode variable to either 'a','s','d' or 'm'.
    Also added some error handling. Program will display an error for zero division error or if an invalid opcode is specified.

--- src/com/vaibhav/calculator/Main.java
+++ src/com/vaibhav/calculator/Main.java
@@ -4,28 +4,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Operands
-        float floatVal = 1.0f;
-        double doubleVal = 4.0d;
-        byte byteVal = 7;
-        short shortVal = 7;
-        long longVal = 5;
-
-        short result1 = (short) (byteVal); // Implicit Conversion
-        short result2 = (short) longVal; // Explicit Conversion
+        // Operands
+        double val1 = 100.0d;
+        double val2 = 50.0d;
+        double result;
 
 
-        //Operations
-        short result3 = (short) (byteVal - longVal); // Explicit Conversion
-        double result4 = longVal - doubleVal;
-        long result5 = (long) (shortVal - longVal + floatVal + doubleVal);
+        // Operators
+        char opCode = 'a';
 
-        //Print Result
-        System.out.println(result1);
-        System.out.println(result2);
-        System.out.println(result3);
-        System.out.println(result4);
-        System.out.println(result5);
+        // Program Logic
+        if(opCode == 'a')
+            result = val1 + val2;
+        else if(opCode == 's')
+            result = val1 - val2;
+        else if(opCode == 'd')
+            if (val2 != 0.0d) // Check for zero division
+                result = val1 / val2;
+            else {
+                System.out.println("Error - zero division error");
+                result = 0.0d;
+            }
+        else if(opCode == 'm')
+            result = val1 * val2;
+        else {
+            System.out.println("Error - invalid opCode"); // Error Handling
+            result = 0.0d;
+        }
+
+        System.out.println(result);
+
 
     }
 }
